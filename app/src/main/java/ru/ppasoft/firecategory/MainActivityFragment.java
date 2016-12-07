20adf504b251337be008698e6adfeedcd9180797
ifuterman <KESZ3r21>
2016-12-07 22:34:13 +0500

    Initial commit

---
+++ app/src/main/java/ru/ppasoft/firecategory/MainActivityFragment.java
@@ -0,0 +1,24 @@
+package ru.ppasoft.firecategory;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends ListFragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
